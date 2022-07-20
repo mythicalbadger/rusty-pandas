3c1ac4b7baa5dd267204a1ed86f971e05c3acbda
mythicalbadger <jussy12.work@gmail.com>
2022-07-20 13:32:52 +0700

    Improved code organization

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,4 @@ fn gen_vec(n: usize) -> Vec<f64> {
 }
 
 fn main() {
-    let df = dataframe::read_csv("./res/2008.csv");
-    println!("Summing across columns");
-    let st = Instant::now();
-    let a = df.sum(0);
-    println!("{:?} to sum across columns", st.elapsed().as_millis());
-    println!("Summing across rows");
-    let st = Instant::now();
-    let a = df.sum(1);
-    println!("{:?} to sum across rows", st.elapsed().as_millis());
-
-    println!("{}", a.size());
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
 }
